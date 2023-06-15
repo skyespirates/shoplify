6ffeee9646169b61ecda5606ab777ab7bb097c60
skyespirates <akashiseijurou414@gmail.com>
2023-06-15 20:04:49 +0700

    initial commit & implement basic OOP

---
+++ src/script.ts
@@ -0,0 +1,107 @@
+interface IProduct {
+  id: number;
+  name: string;
+  price: number;
+  year: number;
+}
+
+class Product implements IProduct {
+  static count: number = 0;
+  private _id: number;
+  constructor(
+    private _name: string,
+    private _price: number,
+    private _year: number
+  ) {
+    this._id = ++Product.count;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public set name(v: string) {
+    this._name = v;
+  }
+
+  public get price(): number {
+    return this._price;
+  }
+
+  public set price(v: number) {
+    this._price = v;
+  }
+
+  public get year(): number {
+    return this._year;
+  }
+
+  public set year(v: number) {
+    this._year = v;
+  }
+
+  public get id(): number {
+    return this._id;
+  }
+
+  public set id(v: number) {
+    this._id = v;
+  }
+}
+
+interface IUI {
+  addProduct(product: Product): void;
+  resetForm(): void;
+  deleteProduct(element: any): void;
+}
+
+class UI implements IUI {
+  addProduct(product: Product): void {
+    const output = document.querySelector(".output") as HTMLDivElement;
+    const element = document.createElement("div") as HTMLDivElement;
+    element.className = "card";
+    element.innerHTML = `
+    <div class="card-body">
+                <h5 class="text-center">${product.name}</h5>
+                <div class="info mx-auto d-flex justify-content-between">
+                  <span> <strong>Price</strong>: Rp ${product.price} </span>
+                  <span> <strong>Year</strong>: ${product.year} </span>
+                    <i role="button" class="ri-delete-bin-7-fill ri-xl text-danger"></i>
+                </div>
+              </div>
+    `;
+    output.appendChild(element);
+  }
+  resetForm(): void {
+    const form = document.querySelector("form") as HTMLFormElement;
+    form.reset();
+  }
+  deleteProduct(element: any): void {
+    const button = element.role;
+
+    if (button === "button") {
+      element.parentElement.parentElement.parentElement.remove();
+      return;
+    }
+  }
+}
+
+const form = document.querySelector("form") as HTMLFormElement;
+form.addEventListener("submit", (e: SubmitEvent) => {
+  e.preventDefault();
+  const name = document.getElementById("product-name") as HTMLInputElement;
+  const price = document.getElementById("product-price") as HTMLInputElement;
+  const year = document.getElementById("product-year") as HTMLInputElement;
+
+  const product = new Product(name.value, +price.value, +year.value);
+  const ui = new UI();
+  ui.addProduct(product);
+  ui.resetForm();
+});
+
+const output = document.querySelector(".output") as HTMLDivElement;
+output.addEventListener("click", (e) => {
+  e.preventDefault();
+  const ui = new UI();
+  ui.deleteProduct(e.target);
+});
